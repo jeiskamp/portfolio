7c4af6499a1b735be7c5b458a93c547f912f5261
jeiskamp <eiskamp7335@gmail.com>
2021-04-28 17:19:33 -0500

    navbar

--- FlaskSite.py
+++ FlaskSite.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
